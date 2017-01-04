503333cffaf862380bb845b3bacd102c13d4d97c
Tim Nelson <tim@eltimn.com>
2017-01-04 09:34:04 -0600

    Release v1.0.0

---
+++ release.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# Manually update package.json first
+
+set -e
+
+# Version key/value should be on his own line
+VERSION=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g' \
+  | tr -d '[[:space:]]')
+
+# git commit and tag
+git add package.json
+git commit -m "Release v${VERSION}"
+git tag v${VERSION}
+
+git push origin master
+git push origin v${VERSION}
